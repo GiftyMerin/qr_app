4c064a9ec0b7595ba7f491f65d83ea94b738604c
Merin <gif@example.com>
2024-03-14 15:44:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:qr_app/login.dart';
+import 'package:qr_app/regis.dart';
+import 'package:qr_app/qrp.dart';
+
+import 'login.dart';
+
+void main() {
+  runApp(const QrApp());
+}
+
+class QrApp extends StatefulWidget {
+  const QrApp({super.key});
+
+  @override
+  State<QrApp> createState() => _QrAppState();
+}
+
+class _QrAppState extends State<QrApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LogIn(),
+    );
+  }
+}
